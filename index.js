211a01d83cc1df19f87ec20e1b908358535bdf29
kushalkumarmolabanti <63012040+kushalkumarmolabanti@users.noreply.github.com>
2022-06-30 13:27:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ btn.addEventListener("click", function(e){
     if(fname.value === ""){
         fname.classList.add("error");
     }
+    else{
+        fname.classList.remove("error");
+    }
     if(lname.value === ""){
         lname.classList.add("error");
+    }
+    else{
+        lname.classList.remove("error");
     }
     if(email.value === ""){
         email.classList.add("error");
     }
+    else{
+        email.classList.remove("error");
+    }
     if(ps.value === ""){
         ps.classList.add("error");
+    }
+    else{
+        ps.classList.remove("error");
     }
 })
